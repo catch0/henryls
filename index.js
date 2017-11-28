d99fded9a71e4415914608194a9686aa77b2f12a
Spence Peacock <spencerpeacock@gmail.com>
2017-11-27 21:25:10 -0600

    mass reply working

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const config = require('./config');
 const twit = require('twit');
 const T = new twit(config);
+const twts = require('./data/tweets.js');
+const tweets = twts.tweets;
 
-//function gets brandnewcongress tweets and will store them as an array of objects
-T.get('statuses/user_timeline', {screen_name: 'realwhchef', language: 'en', include_rts: false, exclude_replies: true } , gotData);
+//function gets users tweets and will store them as an array of objects
+T.get('statuses/user_timeline', {screen_name: 'spencepeacock1', language: 'en', include_rts: false, exclude_replies: true } , gotData);
  function gotData(err, data, response) {
    for(var i = 0; i < data.length; i++){
-     var randomNumber = Math.floor(Math.random() * 20);
-     var statusObj = {status: "@" + data[i].user.screen_name + " ? lol" + randomNumber + " funniest tweet today",
+     var myStatus = tweets[Math.floor(Math.random()*tweets.length)];
+     var statusObj = {status: "@" + data[i].user.screen_name + myStatus,
                 in_reply_to_status_id: data[i].id_str
       }
+      console.log(data[i].user.screen_name);
+      console.log(data[i].)
     T.post('statuses/update', statusObj, function(err,tweetReply, resp){
       if(err){
         console.log("error in posting", err)
