9bfde842c1ac3f3800a3f141c8ffdcd3d916561f
Spence Peacock <spencerpeacock@gmail.com>
2017-12-05 23:26:05 -0600

    got streaming working

--- index.js
+++ index.js
@@ -4,22 +4,41 @@ const T = new twit(config);
 const twts = require('./data/tweets.js');
 const tweets = twts.tweets;
 
-//function gets users tweets and will store them as an array of objects
-T.get('statuses/user_timeline', {screen_name: 'spencepeacock1', language: 'en', include_rts: false, exclude_replies: true } , gotData);
- function gotData(err, data, response) {
-   for(var i = 0; i < data.length; i++){
-     var myStatus = tweets[Math.floor(Math.random()*tweets.length)];
-     var statusObj = {status: "@" + data[i].user.screen_name + myStatus,
-                in_reply_to_status_id: data[i].id_str
-      }
-      console.log(data[i].user.screen_name);
-      console.log(data[i].)
-    T.post('statuses/update', statusObj, function(err,tweetReply, resp){
-      if(err){
-        console.log("error in posting", err)
-      }
-      console.log("it worked!!");
-      console.log(tweetReply.text);
-    });
-  }
+// function gets users tweets and will store them as an array of objects
+
+// T.get('statuses/user_timeline', {screen_name: 'spencepeacock1', language: 'en', include_rts: false, exclude_replies: true, count: 2 } , gotData);
+//  function gotData(err, data, response) {
+//    for(var i = 0; i < data.length; i++){
+//
+//      var statusObj = {status: "@" + data[i].user.screen_name + " @willfisher4cong is running to replace michael burgess join us!",
+//                 in_reply_to_status_id: data[i].id_str
+//       }
+//       if(data[i].created_at < 10)
+//       console.log("tweet" + (i+1));
+//       console.log("Text:  " + data[i].text);
+//       console.log("User:   " + data[i].user.name);
+//       console.log("Time:" + data[i].created_at);
+//       console.log("user id string: " + data[i].id_str);
+//       var date = new Date();
+//       var day = date.getDay();
+//   }
+// }
+
+
+//streams for a specific user
+
+console.log("listening...")
+var stream = T.stream('statuses/filter', {track: "Tim Tams", follow: '932718726172055760'});
+stream.on('tweet', function(tweet, err){
+  console.log("we found a tweet...");
+  var statusObj = {status: "@" + tweet.user.screen_name + " I will find you and force you to eat Tim Tams",
+                in_reply_to_status_id: tweet.id_str
 }
+  T.post('statuses/update', statusObj, function(err,tweetReply, resp){
+    if(err){
+      console.log("error in posting", err)
+    }
+    console.log("it worked!!");
+    console.log(tweetReply.text);
+  });
+});
