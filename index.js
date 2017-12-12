9b31f9ac38b7ba15ede5141c420d016df14de4e8
Spence Peacock <spencerpeacock@gmail.com>
2017-12-11 21:40:45 -0600

    adjsted stream2

--- index.js
+++ index.js
@@ -5,55 +5,55 @@ const twts = require('./data/tweets.js');
 const tweets = twts.tweets;
 
 const spence = '880634071419846657';
-const burgess = '15751083';
+const burgers = '15751083';
 const duckboi = '865898987836014592';
 const henry = '925160703006330880';
-// function gets users tweets and will store them as an array of objects
 
-// T.get('statuses/user_timeline', {screen_name: 'spencepeacock1', language: 'en', include_rts: false, exclude_replies: true, count: 2 } , gotData);
-//  function gotData(err, data, response) {
-//    for(var i = 0; i < data.length; i++){
-//
-//      var statusObj = {status: "@" + data[i].user.screen_name + " @willfisher4cong is running to replace michael burgess join us!",
-//                 in_reply_to_status_id: data[i].id_str
-//       }
-//       if(data[i].created_at < 10)
-//       console.log("tweet" + (i+1));
-//       console.log("Text:  " + data[i].text);
-//       console.log("User:   " + data[i].user.name);
-//       console.log("Time:" + data[i].created_at);
-//       console.log("tweet id: " + data[i].id);
-//       var date = new Date();
-//       var day = date.getDay();
-//   }
-// }
-
-
-//streams for michael burgess and replies
-
+//streams for burgers and replies
 console.log("listening...")
-// var stream = T.stream('statuses/filter', {follow: spence});
-// stream.on('tweet', function(tweet, err){
-//   console.log("we found a tweet...");
-//   console.log(tweet);
-//   var statusObj = {status: "@" + tweet.user.screen_name + " tired of this guy? lets replace him with @willfisher4cong",
-//                 in_reply_to_status_id: tweet.id_str
-// }
-//   T.post('statuses/update', statusObj, function(err,tweetReply, resp){
-//     if(err){
-//       console.log("error in posting", err)
-//     }
-//     console.log("it worked!!");
-//     console.log(tweetReply.text);
-//   });
-// });
+var stream = T.stream('statuses/filter', {follow: burgers});
+stream.on('tweet', function(tweet, err){
+  console.log("we found a tweet...");
+  console.log(tweet);
+  var statusObj = {status: "@" + tweet.user.screen_name + " tired of this guy? lets replace him with @willfisher4cong",
+                in_reply_to_status_id: tweet.id_str
+}
+  T.post('statuses/update', statusObj, function(err,tweetReply, resp){
+    if(err){
+      console.log("error in posting", err)
+    }
+    console.log("it worked!!");
+    console.log(tweetReply.text);
+  });
+});
 
 //streams for someone and retweets
-
-var stream2 = T.stream('statuses/filter', {follow: spence});
+var stream2 = T.stream('statuses/filter', {follow: duckboi});
  stream2.on('tweet', function(tweet, err){
    var retweet = tweet.id_str;
    T.post('statuses/retweet/:id', { id: retweet }, function (err, data, response) {
      console.log(data)
    });
  });
+
+
+
+ // function gets users tweets and will store them as an array of objects
+
+ // T.get('statuses/user_timeline', {screen_name: 'spencepeacock1', language: 'en', include_rts: false, exclude_replies: true, count: 2 } , gotData);
+ //  function gotData(err, data, response) {
+ //    for(var i = 0; i < data.length; i++){
+ //
+ //      var statusObj = {status: "@" + data[i].user.screen_name + " @willfisher4cong is running to replace michael burgess join us!",
+ //                 in_reply_to_status_id: data[i].id_str
+ //       }
+ //       if(data[i].created_at < 10)
+ //       console.log("tweet" + (i+1));
+ //       console.log("Text:  " + data[i].text);
+ //       console.log("User:   " + data[i].user.name);
+ //       console.log("Time:" + data[i].created_at);
+ //       console.log("tweet id: " + data[i].id);
+ //       var date = new Date();
+ //       var day = date.getDay();
+ //   }
+ // }
